cb15f73234801ce4a6071a656b4b44b20697d8b7
KillLaker53 <ssis.kn@gmail.com>
2024-12-07 11:27:57 +0200

    feat: added route for creating an event

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { getUsers, getUser, registerUser } from './controllers/users.controller';
 import { validateUserFields, checkIfUserExists } from './middleware/validate.user';
+import { validateEventFields, checkIfEventExists } from './middleware/validate.event';
 import { handleValidationResult } from './middleware/handle.validation.result';
+import { createEvent } from './controllers/events.controller';
 
 const routes = express.Router();
 
@@ -10,5 +12,7 @@ routes.post('/register', validateUserFields, handleValidationResult, checkIfUser
 routes.get('/getUser', getUser);
 
 routes.get('/getUsers', getUsers);
+
+routes.post('/createEvent', validateEventFields, handleValidationResult, checkIfEventExists, createEvent);
 
 export default routes;
