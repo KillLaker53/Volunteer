fb5264b5ff0d3dced91274c37c87dcba09240df2
KillLaker53 <ssis.kn@gmail.com>
2024-12-07 11:31:28 +0200

    fix: transformed status from type to enum

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,9 +2,12 @@ export enum EventType {
     Fire = 'Fire',
     Flooding = 'Flooding'
 };
+export enum Status{
+    Fire = 'On-Going', 
+    Finished = 'Finished',
+};
 
 export type UserRole = 'Volunteer' | 'Admin';
-export type Status = 'On-Going' | 'Finished';
 export type Location = {
     type: 'Point';
     coordinates: [Number, Number];
