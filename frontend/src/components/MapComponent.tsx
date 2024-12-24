000eab3f7c4f09bbf652eacd91071656b5bc8fea
KillLaker53 <ssis.kn@gmail.com>
2024-12-24 12:10:48 +0200

    feat: added marker logic and custom icons for different types of events

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -1,18 +1,64 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker,  } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from 'react-leaflet';
-import "./MapComponent.css"
+import { EventLocationDto } from 'types-api-volunteer/src';
+import "./MapComponent.css";
+import { Icon } from 'leaflet';
 
-const MapComponent = () => {
-    return(
-        <MapContainer center={[42.7339, 25.4858]} zoom={8} className='map' >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          
-        </MapContainer>
-    );
+interface MapComponentProps {
+  events: EventLocationDto[];
 }
+
+const MapComponent: React.FC<MapComponentProps> = ({ events }) => {
+  if (!events || events.length === 0) {
+    return <div>No events to display on the map.</div>;
+  }
+
+  const fireIcon = new Icon({
+    iconUrl: require("../images/fire.png"),
+    iconSize: [45, 45]
+  })
+
+  const floodingIcon = new Icon({
+    iconUrl: require("../images/flooding.png"),
+    iconSize: [45, 45]
+  })
+
+  const earthquakeIcon = new Icon({
+    iconUrl: require("../images/earthquake.png"),
+    iconSize: [45, 45]
+  })
+
+  return (
+    <MapContainer center={[42.7339, 25.4858]} zoom={8} className="map">
+      <TileLayer
+        attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {events.map((event, index) => {
+
+        let customIcon;
+        switch(event.type.toLowerCase()){
+          case "flooding":
+            customIcon = floodingIcon;
+            break;
+          case "fire":
+            customIcon = fireIcon;
+            break;
+          case "earthquake":
+            customIcon = earthquakeIcon;
+            break;
+        }
+        return (
+          <Marker
+            key={index}
+            position={[event.longitude, event.latitude]}
+            icon={customIcon}
+          ></Marker>
+        );
+      })}
+    </MapContainer>
+  );
+};
 
 export default MapComponent;
