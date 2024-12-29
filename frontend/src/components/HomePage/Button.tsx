69f636cbf7de4acd930bd8b8dce533a036fc81bc
KillLaker53 <ssis.kn@gmail.com>
2024-12-29 18:37:04 +0200

    feat: adjusted button component structure to make it more reusable

--- frontend/src/components/HomePage/Button.tsx
+++ frontend/src/components/HomePage/Button.tsx
@@ -3,13 +3,19 @@ import './Button.css';
 
 interface ButtonProps {
     onClick: () => void;
+    icon?: string;
+    altText?: string;
     children: string;
 }
 
 
-const Button: React.FC<ButtonProps> = ({onClick, children}) => {
+const Button: React.FC<ButtonProps> = ({onClick, icon, altText, children}) => {
     return(
         <button className='button' onClick={onClick}>
+            {
+            icon && 
+            <img src={icon} alt={altText} className='button-icon'/>
+            }
             {children}
         </button>
     );
